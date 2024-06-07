a3209dcdf2ca65deaa25d6f1b0bc0fff8ea1b827
name <no@user.co>
2024-06-07 09:57:45 +0000

    commit

---
+++ bbs/__init__.py
@@ -0,0 +1,27 @@
+import glob
+
+from typing import Union
+
+from starlette.applications import Starlette
+from starlette.routing import Mount
+import uvicorn
+
+import toml
+
+from .core import BBS
+
+#instance_names = ["one", "another"]
+instance_names = ["one"]
+
+routes = list()
+
+for name in instance_names:
+    bbs_application = BBS(instance=name)
+    # if there's only one site, let's put in it root (dev mode/mono mode)
+    # if there are multiple sites, let's put them in "/uri” (multisite/multi
+    #   mode)
+    path = f"/{name}" if len(instance_names) > 1 else "/"
+    mount_point = Mount(path, bbs_application.api)
+    routes.append(mount_point)
+
+app = Starlette(debug=True, routes=routes)
--- bbs/__main__.py
+++ bbs/__main__.py
@@ -1,31 +1,36 @@
-import glob
-
-from typing import Union
-
-from starlette.applications import Starlette
-from starlette.routing import Mount
 import uvicorn
 
-import toml
+#  import glob
+#
+#  from typing import Union
+#
+#  from starlette.applications import Starlette
+#  from starlette.routing import Mount
+#  import uvicorn
 
-from core import BBS
+#  import toml
 
-#instance_names = ["one", "another"]
-instance_names = ["one"]
+#  from . import app
 
-routes = list()
-
-for name in instance_names:
-    bbs_application = BBS(instance=name)
-    # if there's only one site, let's put in it root (dev mode/mono mode)
-    # if there are multiple sites, let's put them in "/uri” (multisite/multi
-    #   mode)
-    path = f"/{name}" if len(instance_names) > 1 else "/"
-    mount_point = Mount(path, bbs_application.api)
-    routes.append(mount_point)
-
-app = Starlette(debug=True, routes=routes)
+#  from core import BBS
+#
+#  #instance_names = ["one", "another"]
+#  instance_names = ["one"]
+#
+#  routes = list()
+#
+#  for name in instance_names:
+#      bbs_application = BBS(instance=name)
+#      # if there's only one site, let's put in it root (dev mode/mono mode)
+#      # if there are multiple sites, let's put them in "/uri” (multisite/multi
+#      #   mode)
+#      path = f"/{name}" if len(instance_names) > 1 else "/"
+#      mount_point = Mount(path, bbs_application.api)
+#      routes.append(mount_point)
+#
+#  app = Starlette(debug=True, routes=routes)
 
 if __name__ == '__main__':
     #  uvicorn.run("bbs.__main__:app", host="0.0.0.0", port=8000, reload=True)
-    uvicorn.run("bbs.__main__:app", host="0.0.0.0", port=8000, reload=True)
+    #  uvicorn.run("bbs.__main__:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("bbs:app", host="0.0.0.0", port=8000, reload=True)

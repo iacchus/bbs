dfbc63d8e88d1f3764712ffadb8b667951d249e6
Iacchus <881531+iacchus@users.noreply.github.com>
2024-07-04 17:39:14 -0300

    commit

--- bbs/authentication.py
+++ bbs/authentication.py
@@ -3,7 +3,17 @@ from litestar.middleware import AuthenticationResult
 
 from litestar.connection import ASGIConnection
 
+from litestar.exceptions import NotAuthorizedException
+
+
+API_KEY_HEADER = "X-API-KEY"
+
+
 class AuthenticationMiddleware(AbstractAuthenticationMiddleware):
     async def authenticate_request(self, connection: ASGIConnection) -> AuthenticationResult:
         #  pass
         #  return await super().authenticate_request(connection)
+
+        auth_header = connection.headers.get(API_KEY_HEADER)
+        if not auth_header:
+            raise NotAuthorizedException()
